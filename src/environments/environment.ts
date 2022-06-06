dfb0575a8c76fe43754a5bcf51ff53a295fa8003
Space0Timer <86170651+Space0Timer@users.noreply.github.com>
2022-06-07 01:59:25 +0800

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,6 @@
 export const environment = {
   production: false,
   firebaseConfig : {
-    apiKey: 'AIzaSyCs6GRnuTKtNtaOX69OOVe9-OttRnvuEro',
-    authDomain: 'chainfox-bae68.firebaseapp.com',
-    projectId: 'chainfox-bae68',
-    storageBucket: 'chainfox-bae68.appspot.com',
-    messagingSenderId: '74998749627',
-    appId: '1:74998749627:web:6623d7c62c0a04e8050c9e',
-    measurementId: 'G-626FR1GTJM'
   }
 };
 
